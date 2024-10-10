892762bd832c5da39f27d87ba4548e8339e38fa5
HatCodeDev <misael.reynoso.aguayo@gmail.com>
2024-10-09 22:24:24 -0600

    feat: Correción de estilos responsivos y eliminacion de logica inservible

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,10 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 Route::middleware('auth')->group(function () {
-    Route::resource('games',App\Http\Controllers\GameController::class);
-    // Otras rutas protegidas
     Route::resource('users',App\Http\Controllers\UserController::class);
     Route::resource('tostados',App\Http\Controllers\TostadoController::class);
     Route::resource('bebidas',App\Http\Controllers\BebidaController::class);
